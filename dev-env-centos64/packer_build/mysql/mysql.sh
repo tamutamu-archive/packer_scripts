27249d5f1c2560e1ee13bff698f0bf3998deb1eb
tamutamu <tamu.0.0.tamu@gmail.com>
2015-12-02 21:46:54 +0900

    Update mysql.sh

--- dev-env-centos64/packer_build/mysql/mysql.sh
+++ dev-env-centos64/packer_build/mysql/mysql.sh
@@ -1,4 +1,4 @@
-sudo yum -y install $MYSQL_IMPL-server mysql-devel
-sudo service ${MYSQL_IMPL}d start
-sudo chkconfig ${MYSQL_IMPL}d on
+yum -y install $MYSQL_IMPL-server mysql-devel
+service ${MYSQL_IMPL}d start
+chkconfig ${MYSQL_IMPL}d on
 /usr/bin/mysqladmin -u root password $MYSQL_ROOT_PASSWORD

5dfa154f02d77bb4b853623e3ace58aaacdfd06b
Your Name <you@example.com>
2015-12-16 13:56:04 +0900

    Modify mysql.

--- centos7.1/packer_build/mysql/mysql.sh
+++ centos7.1/packer_build/mysql/mysql.sh
@@ -2,7 +2,7 @@ wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
 rpm -Uvh mysql-community-release-el7-5.noarch.rpm
 yum -y install mysql-community-server
 
-mysql_install_db
+sudo -u mysql mysql_install_db
 
 systemctl start mysqld.service
 systemctl enable mysqld.service

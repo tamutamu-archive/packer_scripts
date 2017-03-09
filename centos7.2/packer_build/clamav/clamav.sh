810e57349bf15419128822ab9a4f719a0f11dc8a
root <root@localhost.localdomain>
2017-03-09 13:27:11 +0900

    Modify clamav.

--- centos7.2/packer_build/clamav/clamav.sh
+++ centos7.2/packer_build/clamav/clamav.sh
@@ -17,11 +17,18 @@ yum -y install --enablerepo=epel clamav-server clamav-data clamav-update \
 ### Copy Config
 \cp -f "${CURDIR}"/conf/freshclam.conf /etc/
 \cp -f "${CURDIR}"/conf/scan.conf /etc/clamd.d/
+ln -s /etc/clamd.d/scan.conf /etc/clamd.conf
 
 
 ### Service
-systemctl start clamd@scan
+systemctl start clamd@scan.service
 systemctl enable clamd@scan.service
+
+
+### Cron
+mkdir -p /opt/clamav
+\cp -f "${CURDIR}"/conf/clamav.sh /opt/clamav/
+\cp -f "${CURDIR}"/conf/clamav /etc/cron.d/
 
 
 ### Update
